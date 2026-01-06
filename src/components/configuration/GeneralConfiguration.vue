9177fd7a98c745b8ef90286845638ce8ccaa0165
Mohammad Azmi <mohammad.azmi.id@gmail.com>
2026-01-06 17:52:34 +0700

    custom local instructions

--- src/components/configuration/GeneralConfiguration.vue
+++ src/components/configuration/GeneralConfiguration.vue
@@ -1,22 +1,60 @@
 <template>
   <h2>General</h2>
-  <BaseInput :model-value="customInstructions" @change="handleChange" type="textarea"
-    placeholder="E.g. Before running a query, analyze it for any potential issues." rows="4">
-    <template #label>Custom Instructions</template>
-    <template #helper>Custom instructions will be appended to the <ExternalLink href="https://github.com/beekeeper-studio/bks-ai-shell/blob/main/instructions">default instructions</ExternalLink> and included with every message you send as a system prompt. These instructions will be applied globally to all connections.</template>
-  </BaseInput>
-  <BaseInput type="switch" :model-value="allowExecutionOfReadOnlyQueries" @click="handleSwitchClick">
+  <BaseInput
+    :model-value="customInstructions"
+    @change="configure('customInstructions', $event.target.value)"
+    type="textarea"
+    placeholder="E.g. Before running a query, analyze it for any potential issues."
+    rows="4"
+  >
     <template #label>
-      Always allow execution of read-only queries
+      Custom Instructions <span style="color: var(--text-muted)">(Global)</span>
     </template>
+    <template #helper
+      >Custom instructions will be appended to the
+      <ExternalLink
+        href="https://github.com/beekeeper-studio/bks-ai-shell/blob/main/instructions"
+        >default instructions</ExternalLink
+      >
+      and included with every message you send as a system prompt. These
+      instructions will be applied globally to all connections.</template
+    >
+  </BaseInput>
+  <BaseInput
+    :model-value="currentLocalInstructions"
+    @change="setCustomLocalInstructions($event.target.value)"
+    type="textarea"
+    placeholder="E.g. Before running a query, analyze it for any potential issues."
+    rows="4"
+  >
+    <template #label>
+      Custom Instructions <span style="color: var(--text-muted)">(Local)</span>
+    </template>
+    <template #helper
+      >Custom instructions will be appended to the
+      <ExternalLink
+        href="https://github.com/beekeeper-studio/bks-ai-shell/blob/main/instructions"
+        >default instructions</ExternalLink
+      >
+      and included with every message you send as a system prompt. These
+      instructions will be applied locally to the current connection.</template
+    >
+  </BaseInput>
+  <BaseInput
+    type="switch"
+    :model-value="allowExecutionOfReadOnlyQueries"
+    @click="handleSwitchClick"
+  >
+    <template #label> Always allow execution of read-only queries </template>
     <template #helper>
-      This will allow execution of read-only queries without asking for confirmation in all sessions.
+      This will allow execution of read-only queries without asking for
+      confirmation in all sessions.
     </template>
   </BaseInput>
 </template>
 
 <script lang="ts">
-import { mapActions, mapState } from "pinia";
+import { mapActions, mapGetters, mapState } from "pinia";
 import BaseInput from "@/components/common/BaseInput.vue";
 import { useConfigurationStore } from "@/stores/configuration";
 import ExternalLink from "@/components/common/ExternalLink.vue";
@@ -34,18 +72,19 @@ export default {
       "customInstructions",
       "allowExecutionOfReadOnlyQueries",
     ]),
+    ...mapGetters(useConfigurationStore, ["currentLocalInstructions"]),
   },
 
   methods: {
-    ...mapActions(useConfigurationStore, ["configure"]),
-    handleChange(event: Event) {
-      this.configure(
-        "customInstructions",
-        (event.target as HTMLTextAreaElement).value,
-      );
-    },
+    ...mapActions(useConfigurationStore, [
+      "configure",
+      "setCustomLocalInstructions",
+    ]),
     handleSwitchClick() {
-      this.configure("allowExecutionOfReadOnlyQueries", !this.allowExecutionOfReadOnlyQueries);
+      this.configure(
+        "allowExecutionOfReadOnlyQueries",
+        !this.allowExecutionOfReadOnlyQueries,
+      );
     },
   },
 };
